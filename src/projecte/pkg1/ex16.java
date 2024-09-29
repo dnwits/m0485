acfce28c524a65b0ac9efc329ee7a36678e4c68e
Valery Michelle Escobar <v.m.e.c1122002@gmail.com>
2024-09-29 14:14:20 +0200

    16 a medias

---
+++ src/projecte/pkg1/ex16.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package projecte.pkg1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Val
+ */
+public class ex16 {
+    public static void main(String[] args) {
+    double nota;
+    String nota_txt;
+    Scanner u_nota = new Scanner(System.in);
+    System.out.println("Introduzca una nota: ");
+    nota = u_nota.nextDouble();
+        if (0 <= nota && nota < 3) {
+            nota_txt = "Muy deficiente...";
+        } else if (3 <= nota && nota < 5) {
+            nota_txt = "Insuficiente";
+        } else if (5 <= nota && nota < 6) {
+            nota_txt = "Bien";
+        } else if (6 <= nota && nota < 9){
+            nota_txt = "Notable";
+        } else if (9 <= nota && nota <= 10){
+            nota_txt = "Sobresaliente :3";
+        } else {
+            nota_txt = "Nota introducida errónea";
+        }  
+            System.out.println(nota_txt);
+    }
+}
